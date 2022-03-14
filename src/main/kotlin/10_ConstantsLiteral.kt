eb70a0fdbd3bcfac91a681e1b3a46070ac739e99
Puteri Yudani <puteriyudani@gmail.com>
2022-03-14 23:10:25 +0700

    Percobaan Constants Literal

---
+++ src/main/kotlin/10_ConstantsLiteral.kt
@@ -0,0 +1,21 @@
+fun main() {
+    //literal float
+    val floatLiteral: Float = 178.95F
+    println("Contoh Literal Float : $floatLiteral")
+
+    //literal long
+    val oneBillion: Long = 1000000000L
+    println("Contoh Literal Long : $oneBillion")
+
+    //literal heksadesimal diawali dengan 0x
+    val valHeksa = 0x0F
+    println("Contoh Literal Heksadesimal : $valHeksa")
+
+    //literal binary diawali dengan 0b
+    val valBinary = 0b00001011
+    println("Contoh Literal Binary : $valBinary")
+
+    //literal konvensional
+    val notasiKonvensional = 123.5e10
+    println("Contoh Literal Konvensional Floating Point Number : $notasiKonvensional")
+}
