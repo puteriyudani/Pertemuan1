cfd9e4db72fc78b81592781f4cfca276fc6c49c7
Puteri Yudani <puteriyudani@gmail.com>
2022-03-14 23:06:55 +0700

    Percobaan Blok dan Komentar

---
+++ src/main/kotlin/6_BlokDanKomentar.kt
@@ -0,0 +1,14 @@
+//blok mengelompokkan statement, ditandai dengan tanda buka dan tutup kurung kurawal
+fun main() {
+    println("Hello, ini statement di dalam sebuah blok")
+
+    //komentar tidak akan di eksekusi oleh compiler
+    //komentar 1 baris
+    /*
+    komentar multi baris
+    baris kedua komentar
+     */
+    /**
+     * dokumentasi
+     */
+}
