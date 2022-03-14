5494b96cf8a1947d9134be4eb7263f6469ec2092
Puteri Yudani <puteriyudani@gmail.com>
2022-03-14 23:47:57 +0700

    Percobaan Program Caesar String

---
+++ src/main/kotlin/21_ProgramCaesarChiper.kt
@@ -0,0 +1,23 @@
+fun main() {
+    //mengenkripsi sebuah input text String yang diberikan kedalam karakter yang dinaikan atau diturunkan
+
+    //deklarasi (inisiasi) variabel
+    val s = "blackholesarewheregoddividedbyzero"
+    println("Original message:$s")
+    var message = ""
+    var shift = 1
+
+    //pengulangan iterasi di atas karakter
+    for (c in s) {
+        var i = c.toInt()
+        i += shift
+        if (i > 'z'.toInt()) {
+            i -= 26
+        }
+        val char = i.toChar()
+        message += char
+    }
+
+    //tampilkan
+    println("Encrypted message: $message")
+}
