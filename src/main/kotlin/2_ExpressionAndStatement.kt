d37db3f4c0faad2d3ce4352cffaee9419a8f5065
Puteri Yudani <puteriyudani@gmail.com>
2022-03-14 23:03:10 +0700

    Percobaan Expression and Statement

---
+++ src/main/kotlin/2_ExpressionAndStatement.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //expression adalah kombinasi dari operator, fungsi, literal, dan variabel
+    var x = 20 % 4
+    println(x)
+
+    //operasi assignment tidak bisa dimasukkan di dalam statement
+    // karena kotlin akan menganggapnya sebagai statement di dalam statement
+    // while ((rem = a % b) != 0) -> error
+}
