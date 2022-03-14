12cf14191554df688c46690e4b863be15663c47c
Puteri Yudani <puteriyudani@gmail.com>
2022-03-14 23:06:00 +0700

    Percobaan Operator

---
+++ src/main/kotlin/5_Operator.kt
@@ -0,0 +1,15 @@
+fun main() {
+    //contoh operator adalah aritmatika, logika, equality, dan lain-lain
+
+    //operator aritmatika
+
+    //deklarasi variabel a dan b
+    var a = 7
+    var b = 6
+
+    //deklarasi variabel c
+    var c = (a++ + b++) / 2.0
+
+    //tampilkan hasil operasi c+b (tipe data double)
+    println(c + b)
+}
